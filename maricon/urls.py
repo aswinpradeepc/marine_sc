9e64f98e35336470644356e7cdf617ec0bb3fd8a
Aju Roshan <70354172+ajuroshan@users.noreply.github.com>
2025-03-26 20:53:35 +0530

    registration closed

--- maricon/urls.py
+++ maricon/urls.py
@@ -9,7 +9,7 @@ from .views import (IndexView, RegisterView, OtpView, LoginView,
 urlpatterns = [
     path('', IndexView.as_view(), name='maricon'),
     # path('committee/', CommitteeView.as_view(), name='committee'),
-    path('register/', RegisterView.as_view(), name='register'),
+    # path('register/', RegisterView.as_view(), name='register'),
     path('register/otp/', OtpView.as_view(), name='otp'),
     path('login/otp/', OtpView.as_view(), name='otp'),
     path('abstract/', submission_view, name='submission'),
